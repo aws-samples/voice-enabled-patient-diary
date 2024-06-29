2a60f3010715dfbd4fe5ba5e78b17fc23f5155f0
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-06-28 17:12:29 -0700

    chore: upgrade CDKv2 (#26)

--- lib/common.ts
+++ lib/common.ts
@@ -1,7 +1,8 @@
-import core = require('@aws-cdk/core');
+import core = require('aws-cdk-lib');
+import constructs = require('constructs');
 
 export class Common {
-    public static output(obj: core.Construct, name: string, value: string, desc: string) {
+    public static output(obj: constructs.Construct, name: string, value: string, desc: string) {
         new core.CfnOutput(obj, name, {
                 value: value,
                 description: desc
